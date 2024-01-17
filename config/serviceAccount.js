c69d31c9880818af0de955871bce031df2e8d0e9
ACER <dnhan2426@gmail.com>
2024-01-17 22:55:40 +0700

    fix env firebase

--- config/serviceAccount.js
+++ config/serviceAccount.js
@@ -4,7 +4,7 @@ module.exports = {
   "type": process.env.FIREBASE_TYPE,
   "project_id": process.env.FIREBASE_PROJECTID,
   "private_key_id": process.env.FIREBASE_PRIVATEKEYID,
-  "private_key": process.env.FIREBASE_PRIVATEKEY,
+  "private_key": process.env.FIREBASE_PRIVATEKEY.split(String.raw`\n`).join('\n'),
   "client_email": process.env.FIREBASE_CLIENTEMAIL,
   "client_id": process.env.FIREBASE_CLIENTID,
   "auth_uri": process.env.FIREBASE_AUTHURI,
